6b71c38265ba1c26a0c619a72cab0b1c0256c122
nbintang <nbintangh@gmail.com>
2026-03-03 14:47:52 +0700

    chore: update src/auth/__init__.py

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,3 +1,4 @@
-from src.auth.jwt import decode_and_verify_jwt, require_jwt
+from src.auth.jwt import decode_and_verify_jwt, issue_client_access_token, require_jwt
+from src.auth.revocation import revoke_token
 
-__all__ = ["decode_and_verify_jwt", "require_jwt"]
+__all__ = ["decode_and_verify_jwt", "issue_client_access_token", "require_jwt", "revoke_token"]
